91973e0bdd18b9ef111fc7bd70def3a912b131df
Jose Moreira <jose.moreira.um@gmail.com>
2024-03-19 15:43:58 +0000

    TPC5

--- TPC5/spacy_test.py
+++ TPC5/spacy_test.py
@@ -1,23 +1,15 @@
 import spacy
 
-nlp = spacy.load('pt_core_news_lg')
+nlp = spacy.load("pt_core_news_lg")
 
-text = "O Daniel e o André foram a Ponte de Lima a pé."
-doc = nlp(text)
-aux_ents = dict()
-
-for ent in doc.ents:
-    if ' ' in str(ent):
-        rpc_ent = str(ent).replace(' ', '_')
-        text = text.replace(str(ent), rpc_ent)
-        aux_ents[rpc_ent] = ent.label_, ent.lemma_
-
+text = ('''O Daniel e o André foram a Ponte de Lima a pé.''')
 doc = nlp(text)
 
-tokens = [(str(token), token.pos_, token.lemma_) if str(token) not in aux_ents else (str(token).replace('_', ' '), aux_ents[str(token)][0], aux_ents[str(token)][1]) for token in doc]
-
- 
+with doc.retokenize() as retokenizer:
+    for entity in doc.ents:
+        retokenizer.merge(entity)
+    
 print("TOKEN  ---  POS  ---  LEMMA")
 print('-' * 27)
-for token in tokens:
-    print(f"{token[0]}  ---  {token[1]}  ---  {token[2]}")
+for token in doc:
+    print(f"{str(token)}  ---  {token.pos_}  ---  {token.lemma_}")
